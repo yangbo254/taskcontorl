554da3b6877f83ff2673e385a7e530571eaab318
yangbo <yangbo@yungengxin.com>
2023-04-11 16:07:33 +0800

    添加循环队列逻辑

--- node/taskmgr.go
+++ node/taskmgr.go
@@ -1,6 +1,7 @@
 package node
 
 import (
+	"log"
 	"main/model"
 	"sync"
 	"time"
@@ -13,10 +14,6 @@ type NodeTaskMgr struct {
 	endTaskList     map[uint]model.NodeTaskInfo
 }
 
-type taskRunningInfo struct {
-	ContainerId string
-}
-
 func (mgr *NodeTaskMgr) LoopTask() {
 	// 遍历等待队列，修改status
 	mgr.waitlocker.Lock()
@@ -24,32 +21,72 @@ func (mgr *NodeTaskMgr) LoopTask() {
 		if v.RealStatus == "new" {
 			// 新加入队列的任务
 			// 需要执行pull命令/下载mount文件等前置任务
-			// to do
-
+			// 1.pull镜像
+			_, err := NewDockerCmd().PullImage(v.TaskImageName)
+			if err != nil {
+				log.Fatalf("pull docker images failed. %v", err)
+				delete(mgr.waitTaskList, v.ID)
+			}
+			// 2.下载生成mounts
+			volumes, err := NewNodeUtilDefault().GetFile(v.TaskVolume)
+			if err != nil {
+				log.Fatalf("download volume files failed. %v", err)
+				delete(mgr.waitTaskList, v.ID)
+			}
+			v.RealNodeVolumes = volumes
 			// 切换状态为等待
 			v.RealStatus = "wait"
 		} else if v.RealStatus == "wait" {
 			if time.Now().After(v.Start) {
 				// 切换任务到运行队列
-				// to do
-
-				// 从队列中移除
+				containerId, err := NewDockerCmd().CreateContainer(v.TaskImageName, v.TaskCmd, v.RealNodeVolumes)
+				if err != nil {
+					log.Fatalf("run docker failed. %v", err)
+					delete(mgr.waitTaskList, v.ID)
+				}
+				v.RealNodeContainerId = containerId
+				v.RealStart = time.Now()
+				// swap map
+				mgr.runningTaskList[v.ID] = v
 				delete(mgr.waitTaskList, v.ID)
-
 			}
 		}
 	}
 	mgr.waitlocker.Unlock()
 
+	containers, err := NewDockerCmd().ListContainer()
+	if err != nil {
+		return
+	}
+	containerMap := NewDockerCmd().ConvContainersToMap(containers)
+
 	// 遍历运行队列，等待运行结束
 	for _, v := range mgr.runningTaskList {
-		// v未找到，移入结束队列
-		// to do
+		if vv, found := containerMap[v.RealNodeContainerId]; found {
+			v.RealStatus = vv.State
+			if vv.State == "exited" {
+				// 任务结束，移入结束队列
+				NewDockerCmd().RmContainer(vv.ID)
+				v.RealEnd = time.Now()
+				mgr.endTaskList[v.ID] = v
+				delete(mgr.runningTaskList, v.ID)
+			} else {
+				continue
+			}
+		} else {
+			// v未找到，移入结束队列
+			v.RealStatus = "not found"
+			NewDockerCmd().RmContainer(v.RealNodeContainerId)
+			v.RealEnd = time.Now()
+			mgr.endTaskList[v.ID] = v
+			delete(mgr.runningTaskList, v.ID)
+		}
+
 	}
 
 	// 遍历结束队列，上报结束状态
-	for _, v := range mgr.endTaskList {
-		// 上报v状态，移除自己
-		// to do
-	}
+	//for _, v := range mgr.endTaskList {
+	// 上报v状态，移除自己
+	// to do
+	//}
 }
